9581b6362463945425fb394a2a56b85d91d1dfc0
Hans Ott <hansott@hotmail.be>
2025-12-18 17:14:06 +0100

    Use node version in the name

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const fs = require('fs');
 
-// Load the correct prebuilt binary for this platform/arch
+// Load the correct prebuilt binary for this platform/arch/node version
 const platform = process.platform;
 const arch = process.arch;
+const nodeMajor = process.versions.node.split('.')[0];
 
-const bindingPath = path.join(__dirname, 'prebuilds', `${platform}-${arch}`, 'zen-internals-node.node');
+const bindingPath = path.join(__dirname, 'prebuilds', `${platform}-${arch}`, `zen-internals-node.node${nodeMajor}.node`);
 
 if (!fs.existsSync(bindingPath)) {
   throw new Error(
